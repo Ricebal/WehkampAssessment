93b23bef15f59b9e1d08505544b8ccde85c8c663
Nicander <nicanderbusiness@gmail.com>
2023-03-11 14:30:30 +0100

    Fixed ;

--- Models/Basket.cs
+++ Models/Basket.cs
@@ -4,6 +4,6 @@ public class Basket
 {
     public string SessionId { get; set; }
     public BasketItem[] Items { get; set; }
-    public int ShippingCosts { get; set}
+    public int ShippingCosts { get; set; }
     public int TotalPrice { get; set; }
 }
